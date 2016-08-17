46bfae8814fcb02559c0a04572a4e876c990a27a
Jorge Davison <jdavison@3cinteractive.com>
2016-08-17 13:56:37 -0300

    Stable version of coffeebot

--- index.js
+++ index.js
@@ -37,160 +37,63 @@ This bot demonstrates many of the core features of Botkit:
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 
-if (!process.env.TOKEN) {
-    console.log('Error: Specify token in environment');
+if (!process.env.TOKEN || !process.env.PARTICLE_TOKEN || !process.env.PARTICLE_DEVICE_ID || !process.env.SLACK_CHANNEL) {
+    console.log('Error: Specify tokens in environment');
     process.exit(1);
 }
 
 var Botkit = require('botkit/lib/Botkit.js');
 var os = require('os');
+var requestify = require('requestify');
+var eventsource = require('eventsource');
+var Q = require('q');
 
 var controller = Botkit.slackbot({
     debug: true
 });
 
+var slackChannel = null; //#coffee
+var messages = {
+    MESSAGE_TEXT_BREWING: 'I am preparing some coffee, stay tuned, I will let you know when is ready.',
+    MESSAGE_TEXT_RUNNING_LOW: 'We have no coffee, sorry, but you can go to the kitchen and prepare more!',
+    MESSAGE_TEXT_RUNNING_NORMAL: 'I still have coffee, but not for long... hurry!',
+    MESSAGE_TEXT_RUNNING_FULL: 'I’m full of coffee, run and take you dosis!'
+};
+var particle = {
+    TEMP_URL: 'https://api.particle.io/v1/devices/' + process.env.PARTICLE_DEVICE_ID + '/temp?access_token=' + process.env.PARTICLE_TOKEN,
+    STATUS_URL: 'https://api.particle.io/v1/devices/' + process.env.PARTICLE_DEVICE_ID + '/status?access_token=' + process.env.PARTICLE_TOKEN,
+    LEVEL_URL: 'https://api.particle.io/v1/devices/' + process.env.PARTICLE_DEVICE_ID + '/level?access_token=' + process.env.PARTICLE_TOKEN,
+    EVENTS_URL: 'https://api.particle.io/v1/devices/' + process.env.PARTICLE_DEVICE_ID + '/events?access_token=' + process.env.PARTICLE_TOKEN
+}
+
+
 var bot = controller.spawn({
     token: process.env.TOKEN
-}).startRTM();
+}).startRTM(function (err, bot) {
+    if (err) {
+        throw new Error(err);
+    }
+
+    // @ https://api.slack.com/methods/channels.list
+    bot.api.channels.list({}, function (err, response) {
+        if (response.hasOwnProperty('channels') && response.ok) {
+            var total = response.channels.length;
+            for (var i = 0; i < total; i++) {
+                var channel = response.channels[i];
+                if (channel.name === process.env.SLACK_CHANNEL) {
+                    slackChannel = channel;
+                }
+            }
+        }
+        if (slackChannel == null) {
+            throw new Error('Channel '+ process.env.SLACK_CHANNEL + ' not found');
+        }
+    });
+});
 
 controller.setupWebserver(process.env.PORT,function(err,webserver) {
   controller.createWebhookEndpoints(controller.webserver);
 });
-
-
-controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err);
-        }
-    });
-
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
-    });
-});
-
-controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var name = message.match[1];
-    controller.storage.users.get(message.user, function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user, function(err, id) {
-            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-        });
-    });
-});
-
-controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    controller.storage.users.get(message.user, function(err, user) {
-        if (user && user.name) {
-            bot.reply(message, 'Your name is ' + user.name);
-        } else {
-            bot.startConversation(message, function(err, convo) {
-                if (!err) {
-                    convo.say('I do not know your name yet!');
-                    convo.ask('What should I call you?', function(response, convo) {
-                        convo.ask('You want me to call you `' + response.text + '`?', [
-                            {
-                                pattern: 'yes',
-                                callback: function(response, convo) {
-                                    // since no further messages are queued after this,
-                                    // the conversation will end naturally with status == 'completed'
-                                    convo.next();
-                                }
-                            },
-                            {
-                                pattern: 'no',
-                                callback: function(response, convo) {
-                                    // stop the conversation. this will cause it to end with status == 'stopped'
-                                    convo.stop();
-                                }
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            }
-                        ]);
-
-                        convo.next();
-
-                    }, {'key': 'nickname'}); // store the results in a field called nickname
-
-                    convo.on('end', function(convo) {
-                        if (convo.status == 'completed') {
-                            bot.reply(message, 'OK! I will update my dossier...');
-
-                            controller.storage.users.get(message.user, function(err, user) {
-                                if (!user) {
-                                    user = {
-                                        id: message.user,
-                                    };
-                                }
-                                user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user, function(err, id) {
-                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
-                                });
-                            });
-
-
-
-                        } else {
-                            // this happens if the conversation ended prematurely for some reason
-                            bot.reply(message, 'OK, nevermind!');
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
-
-
-controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-
-        convo.ask('Are you sure you want me to shutdown?', [
-            {
-                pattern: bot.utterances.yes,
-                callback: function(response, convo) {
-                    convo.say('Bye!');
-                    convo.next();
-                    setTimeout(function() {
-                        process.exit();
-                    }, 3000);
-                }
-            },
-        {
-            pattern: bot.utterances.no,
-            default: true,
-            callback: function(response, convo) {
-                convo.say('*Phew!*');
-                convo.next();
-            }
-        }
-        ]);
-    });
-});
-
 
 controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
     'direct_message,direct_mention,mention', function(bot, message) {
@@ -201,7 +104,6 @@ controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your na
         bot.reply(message,
             ':robot_face: I am a bot named <@' + bot.identity.name +
              '>. I have been running for ' + uptime + ' on ' + hostname + '.');
-
     });
 
 function formatUptime(uptime) {
@@ -221,3 +123,51 @@ function formatUptime(uptime) {
     uptime = uptime + ' ' + unit;
     return uptime;
 }
+
+
+/**
+ * COFFEE MAKER ACTIONS
+ */
+
+// Listen for status keyword
+controller.hears(['status'], 'direct_message,direct_mention,mention', function(bot, message) {
+    var temperature = 0, status = 'error', level = 10;
+
+    // Making it concurrent
+    Q.all([
+        requestify.get(particle.TEMP_URL),
+        requestify.get(particle.STATUS_URL),
+        requestify.get(particle.LEVEL_URL)
+    ]).then(function(data){
+        var temperature = data[0].getBody().result;
+        var status = data[1].getBody().result;
+        var level = data[2].getBody().result;
+
+        if (level <= 10) {
+            text = messages.MESSAGE_TEXT_RUNNING_LOW;
+        } else if (level > 10 && level < 90) {
+            text = messages.MESSAGE_TEXT_RUNNING_NORMAL;
+        } else {
+            text = messages.MESSAGE_TEXT_RUNNING_FULL;
+        }
+        bot.reply(message, text + ' :coffee: ' + status + ' :thermometer: ' + temperature + 'ºC :level_slider: ' + level + '%');
+    });
+});
+
+// Listen for Particle events
+controller.on('rtm_open', function(bot) {
+    console.log("Listening on " + particle.EVENTS_URL + " ...");
+    var es = new eventsource(particle.EVENTS_URL);
+    es.addEventListener('STATUS', function(e){ 
+        var event = JSON.parse(e.data);
+        console.log("Event received: " + event);
+        if (event.data === "BREWING") {
+            console.log('Send status change to channel ' + event.data);
+            bot.say({
+                text: messages.MESSAGE_TEXT_BREWING,
+                channel: slackChannel.id
+            });
+        }
+    }, false);
+});
+
